4ecbd60bb598afc38b653ea2f257fd2e7b2ac71b
Brian <juarezgarciabrianatonio@gmail.com>
2025-03-04 19:40:38 -0600

    proyecto Alfabetizacion inicio version 1.1

---
+++ AlphaLearn_Project/src/main/java/alphalearnproject/AlphaLearnProject/AlphaLearn_Project.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package alphalearnproject.AlphaLearnProject;
+
+/**
+ *
+ * @author juare
+ */
+public class AlphaLearn_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("commit");
+    }
+   
+}
