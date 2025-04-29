a119a83015174ab71f79a4ffc33dff44eede42cd
Brian <juarezgarciabrianatonio@gmail.com>
2025-04-29 13:45:55 -0600

    COrrecion del nivel 1.1

--- AlphaLearn_Project/src/main/java/controlador/ControlActividad1.java
+++ AlphaLearn_Project/src/main/java/controlador/ControlActividad1.java
@@ -164,15 +164,14 @@ public class ControlActividad1 extends AbstractSonido implements ActionListener 
             }
            }else if(e.getSource() == this.objActividad.getjButton_Siguiente()){
                reproducirSonido("/resource/sounds/burbuja.wav");
-                toces++;
+                
                 if (indicador < paneles.length - 1) {         
                 indicador++;
                 panel(paneles[indicador]);
                 } else {
-
                     indicador = 0;
                     panel(paneles[indicador]);
-                    toces = 0;
+                    
                 }
                 
                 if(indicador==1){
